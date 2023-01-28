956c0f1eeb900cc58dea21c8f5d0ef92a15e54bf
robertoooc <robertooo.c48@gmail.com>
2023-01-27 20:08:48 -0800

    able to broadcast messages from others and set them in state

--- src/components/pages/Chat.jsx
+++ src/components/pages/Chat.jsx
@@ -1,25 +1,54 @@
-import { io } from "socket.io-client"
-import { useState } from "react"
-const socket = io.connect(`${process.env.REACT_APP_SERVER_URL}`)
+import { io } from "socket.io-client";
+import { useState, useEffect } from "react";
+const socket = io.connect(`${process.env.REACT_APP_SERVER_URL}`);
 export default function Chat() {
-	const [comment, setComment] = useState('')
-	const message = (e) => {
-		e.preventDefault()
-		console.log('click')
-		socket.emit('send_comment', { comment: comment })
-	}
-	return (
-		<div className="home">
-			hello from Chat
-			<form onSubmit={message}>
-				<input
-					type='text'
-					id='message'
-					placeholder='message ...'
-					onChange={e => setComment(e.target.value)}
-				/>
-			</form>
-			<button onClick={message}>connect</button>
+  const [currentUserComment, setCurrentUserComment] = useState([]);
+  const[comment,setComment]=useState('')
+  const [otherUserComment, setOtherUserComment] = useState([]);
+  const message = (e) => {
+    e.preventDefault();
+    console.log(e);
+	setCurrentUserComment([...currentUserComment,comment])
+    socket.emit("send-comment", { comment: currentUserComment });
+  };
+  useEffect(() => {
+    socket.on("receive-comment", (comment) => {
+      setOtherUserComment([...otherUserComment,comment.comment]);
+	  //alert(comment.comment)
+    });
+  }, [socket]);
+  let currentUserComments = currentUserComment.map((comment,idx)=>{
+	return(
+		<div key={`comment-${idx}`}>	
+		<p>{comment}</p>
 		</div>
 	)
+  })
+  let otherUserComments = otherUserComment.map((comment,idx)=>{
+	return(
+		<div key={`otherUserComment-${idx}`}>	
+		<p>{comment}</p>
+		</div>
+	)
+  })
+  return (
+    <div className="home">
+	<h1><strong>Chat Room</strong></h1>
+      <p>your comment:</p>
+		{currentUserComments}
+      <p>other user:</p>
+	  {otherUserComments}
+      <form onSubmit={message}>
+        <input
+		  autoComplete="off"
+          type="text"
+          id="message"
+          placeholder="message ..."
+		  value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+      </form>
+      <button onClick={message}>connect</button>
+    </div>
+  );
 }
