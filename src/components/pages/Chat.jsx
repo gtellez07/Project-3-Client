ee027a8dc68911d5dad2cbb0bb44c3a9b0396cbb
robertoooc <robertooo.c48@gmail.com>
2023-01-28 10:07:30 -0800

    implemented broadcast feature to everyone on the same page interacting

--- src/components/pages/Chat.jsx
+++ src/components/pages/Chat.jsx
@@ -3,48 +3,55 @@ import { useState, useEffect } from "react";
 const socket = io.connect(`${process.env.REACT_APP_SERVER_URL}`);
 export default function Chat() {
   const [currentUserComment, setCurrentUserComment] = useState([]);
-  const[comment,setComment]=useState('')
+  const [comment, setComment] = useState("");
   const [otherUserComment, setOtherUserComment] = useState([]);
+  
   const message = (e) => {
     e.preventDefault();
     console.log(e);
-	setCurrentUserComment([...currentUserComment,comment])
+    setCurrentUserComment([...currentUserComment, comment]);
     socket.emit("send-comment", { comment: currentUserComment });
   };
+
   useEffect(() => {
     socket.on("receive-comment", (comment) => {
-      setOtherUserComment([...otherUserComment,comment.comment]);
-	  //alert(comment.comment)
+      setOtherUserComment([...otherUserComment, comment.comment]);
+      //alert(comment.comment)
     });
   }, [socket]);
-  let currentUserComments = currentUserComment.map((comment,idx)=>{
-	return(
-		<div key={`comment-${idx}`}>	
-		<p>{comment}</p>
-		</div>
-	)
-  })
-  let otherUserComments = otherUserComment.map((comment,idx)=>{
-	return(
-		<div key={`otherUserComment-${idx}`}>	
-		<p>{comment}</p>
-		</div>
-	)
-  })
+
+  let currentUserComments = currentUserComment.map((comment, idx) => {
+    return (
+      <div key={`comment-${idx}`}>
+        <p>{comment}</p>
+      </div>
+    );
+  });
+
+  let otherUserComments = otherUserComment.map((comment, idx) => {
+    return (
+      <div key={`otherUserComment-${idx}`}>
+        <p>{comment}</p>
+      </div>
+    );
+  });
+
   return (
     <div className="home">
-	<h1><strong>Chat Room</strong></h1>
+      <h1>
+        <strong>Chat Room</strong>
+      </h1>
       <p>your comment:</p>
-		{currentUserComments}
+      {currentUserComments}
       <p>other user:</p>
-	  {otherUserComments}
+      {otherUserComments}
       <form onSubmit={message}>
         <input
-		  autoComplete="off"
+          autoComplete="off"
           type="text"
           id="message"
           placeholder="message ..."
-		  value={comment}
+          value={comment}
           onChange={(e) => setComment(e.target.value)}
         />
       </form>
