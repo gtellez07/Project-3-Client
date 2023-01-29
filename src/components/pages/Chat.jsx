3ead56839c4cfeb4c1559825bd969c91e5e13e9f
robertoooc <robertooo.c48@gmail.com>
2023-01-29 13:48:23 -0800

    some success on pinging API for query search of chat names, need to find way to display searches

--- src/components/pages/Chat.jsx
+++ src/components/pages/Chat.jsx
@@ -1,10 +1,13 @@
 import { io } from "socket.io-client";
 import { useState, useEffect } from "react";
+import axios from 'axios'
 const socket = io.connect(`${process.env.REACT_APP_SERVER_URL}`);
 export default function Chat() {
   const [currentUserComment, setCurrentUserComment] = useState([]);
   const [comment, setComment] = useState("");
   const [otherUserComment, setOtherUserComment] = useState([]);
+  const [search,setSearch] = useState('')
+  const[showSearch,setShowSearch]= useState(false)
 
   const message = (e) => {
     e.preventDefault();
@@ -13,12 +16,33 @@ export default function Chat() {
     socket.emit("send-comment", { comment: currentUserComment });
   };
 
+  const handleSearch = async (e)=>{
+    e.preventDefault()
+    try{
+      const searchFor = await axios.get(`${process.env.REACT_APP_SERVER_URL}chats?search=${search}`)
+      console.log(searchFor.data)
+      setShowSearch(!showSearch)
+    //   if(showSearch){
+
+    //     return(
+    //       <div>
+    //     <p>{searchFor.data.title}</p>
+    //     </div>
+    //   )
+    // }
+
+    }catch(err){
+      console.warn(err)
+    }
+
+  }
+
   useEffect(() => {
     socket.on("receive-comment", (comment) => {
       setOtherUserComment([...otherUserComment, comment.comment]);
       //alert(comment.comment)
     });
-  }, []);
+  }, [socket]);
 
   let currentUserComments = currentUserComment.map((comment, idx) => {
     return (
@@ -54,8 +78,18 @@ export default function Chat() {
           value={comment}
           onChange={(e) => setComment(e.target.value)}
         />
+      <button type = 'submit' >connect</button>
       </form>
-      <button onClick={message}>connect</button>
+      <form onSubmit={handleSearch}>
+        <label htmlFor="search">Search Chat</label>
+        <input
+          id="search"
+          type='text'
+          value={search}
+          onChange={(e)=>setSearch(e.target.value)}
+          />
+          <button type="submit">Search</button>
+      </form>
     </div>
   );
 }
