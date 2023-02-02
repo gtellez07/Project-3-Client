19f86d5fa0c9457a9ac0e5ae6aeb6e5ef675eabc
YOUR-USERNAME <gtellez07@yahoo.com>
2023-02-01 20:21:02 -0600

    added delete button

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -44,6 +44,40 @@ export default function Profile({ currentUser, handleLogout }) {
 		fetchData()
 	}, []) // only fire on the first render of this component
 
+	//delete button
+	const handleDelete = async () => {
+		try {
+			// get the token from local storage
+			const token = localStorage.getItem('jwt')
+			// make the auth headers
+			const options = {
+				headers: {
+					'Authorization': token
+				}
+			}
+			// hit the auth locked endpoint
+			const response = await axios.delete(`${process.env.REACT_APP_SERVER_URL}users/${currentUser.id}`, options)
+			// example POST with auth headers (options are always last argument)
+			// await axios.post(url, requestBody (form data), options)
+			// set the secret user message in state
+			setMsg(response.data.msg)
+			console.log(response.data.msg)
+			handleLogout()
+			navigate('/login')
+		} catch (err) {
+			// if the error is a 401 -- that means that auth failed
+			console.warn(err)
+			if (err.response) {
+				if (err.response.status === 401) {
+					// panic!
+					handleLogout()
+					// send the user to the login screen
+					navigate('/login')
+				}
+			}
+		}
+	}
+
 
 	return (
 		<section className="hero is-large">
@@ -63,6 +97,11 @@ export default function Profile({ currentUser, handleLogout }) {
 					</div>
 					<div className='field is-grouped is-grouped-centered'>
 						<h2 className='subtitle is-5'>Thank You for Using CHAPPIE!</h2>
+					</div>
+					<div className="profile">
+						<div className="field is-grouped is-grouped-centered">
+							<button className="button is-danger" onClick={handleDelete}>Delete Account</button>
+						</div>
 					</div>
 				</div>
 			</section>
